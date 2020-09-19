bdd9a8e07a84866e8dc2c96de7954cac20373745
laisk <laisk1@hotmail.com>
2020-09-19 23:18:01 +0300

    @PostMapping realisation

--- src/main/java/com/example/millstones/GreetingController.java
+++ src/main/java/com/example/millstones/GreetingController.java
@@ -1,19 +1,40 @@
 package com.example.millstones;
 
+import com.example.millstones.domain.Message;
+import com.example.millstones.repos.MessageRepo;
+import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Controller;
 import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.RequestParam;
+
+import java.util.Iterator;
+import java.util.Map;
 
 @Controller
 public class GreetingController {
+	@Autowired
+	private MessageRepo messageRepo;
 
 	@GetMapping("/greeting")
 	public String greeting(@RequestParam(name="name", required=false,
-			defaultValue=" Dude, party down.") String name, Model model)
+			defaultValue=" Dude, party down.") String name, Map<String, Object> model)
 	{
-		model.addAttribute("name", name);
+		model.put("name", name);
 		return "greeting";
 	}
+	@GetMapping()
+	public String main(Map<String, Object> model){
+		Iterable<Message> messages = messageRepo.findAll();
+		model.put("messages",messages);
+		return "main";
+	}
+
+	@PostMapping()    //@RequestParam taking from .mustache form or from URl if GET is going
+	public String add(@RequestParam String text, @RequestParam String tag, Map<String, Object> model){
+		return "main";
+	}
+
 
 }
