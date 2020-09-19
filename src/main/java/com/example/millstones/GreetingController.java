3f6bf82dd142c79f2d1771c9aadc4016b46e4ef7
laisk <laisk1@hotmail.com>
2020-09-20 01:16:59 +0300

    Browser Output  realisation

--- src/main/java/com/example/millstones/GreetingController.java
+++ src/main/java/com/example/millstones/GreetingController.java
@@ -14,27 +14,46 @@ import java.util.Map;
 
 @Controller
 public class GreetingController {
-	@Autowired
-	private MessageRepo messageRepo;
+    @Autowired
+    private MessageRepo messageRepo;
 
-	@GetMapping("/greeting")
-	public String greeting(@RequestParam(name="name", required=false,
-			defaultValue=" Dude, party down.") String name, Map<String, Object> model)
-	{
-		model.put("name", name);
-		return "greeting";
-	}
-	@GetMapping()
-	public String main(Map<String, Object> model){
-		Iterable<Message> messages = messageRepo.findAll();
-		model.put("messages",messages);
-		return "main";
-	}
+    @GetMapping("/greeting")
+    public String greeting(@RequestParam(name = "name", required = false,
+            defaultValue = " Dude, party down.") String name, Map<String, Object> model) {
+        model.put("name", name);
+        return "greeting";
+    }
 
-	@PostMapping()    //@RequestParam taking from .mustache form or from URl if GET is going
-	public String add(@RequestParam String text, @RequestParam String tag, Map<String, Object> model){
-		return "main";
-	}
+    @GetMapping()
+    public String main(Map<String, Object> model) {
+        Iterable<Message> messages = messageRepo.findAll();
+        model.put("messages", messages);
+        return "main";
+    }
+
+    @PostMapping()    //@RequestParam taking from .mustache form or from URl if GET is going
+    public String add(@RequestParam String text, @RequestParam String tag, Map<String, Object> model) {
+        Message message = new Message(text, tag);   //saving
+        messageRepo.save(message);
+        Iterable<Message> messages = messageRepo.findAll();  //putting in model and transferring to user
+        model.put("messages", messages);
+        return "main";
+    }
+
+    @PostMapping("filter")
+    public String filter(@RequestParam String filter, Map<String, Object> model) {
+        Iterable<Message> messages;
+
+        if (filter != null && !filter.isEmpty()) {
+            messages = messageRepo.findByTag(filter);
+        } else {
+            messages = messageRepo.findAll();
+        }
+
+        model.put("messages", messages);
+
+        return "main";
+    }
 
 
 }
--- src/main/java/com/example/millstones/repos/MessageRepo.java
+++ src/main/java/com/example/millstones/repos/MessageRepo.java
@@ -3,6 +3,9 @@ package com.example.millstones.repos;
 import com.example.millstones.domain.Message;
 import org.springframework.data.repository.CrudRepository;
 
+import java.util.List;
+
 public interface MessageRepo extends CrudRepository<Message, Long>{
+    List<Message> findByTag(String tag);
 
 }
